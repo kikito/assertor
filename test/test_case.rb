d30472a4c8a1f3004b7fa405deee33dd8ceaa685
Enrique García Cota <kikito@gmail.com>
2011-05-22 16:04:55 +0200

    removed unused tests

--- test/test_case.rb
+++ test/test_case.rb
@@ -2,16 +2,4 @@ require 'helper'
 
 class TestCase < Assertor::Case
 
-  def test_run
-    c = Class.new Assertor::Case
-    c = Run
-  end
-
-
-  def test_assert_without_message
-    assert(true)
-  end
-
-
-
 end
