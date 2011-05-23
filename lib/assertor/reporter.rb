cfb2f413898e5a36d44678aa299dc2192285c41c
Enrique García Cota <kikito@gmail.com>
2011-05-24 01:03:18 +0200

    reporter is nearly done ...

--- lib/assertor/reporter.rb
+++ lib/assertor/reporter.rb
@@ -2,14 +2,43 @@ module Assertor
 
   class Reporter
 
-    def initialize(stream, cases_list)
-      @stream = stream
+    def initialize(io, cases_list)
+      @io = io
       @cases_list = cases_list
+
+      @passed, @failed, @errors, @exceptions = [],[],[],{}
     end
 
     def run
-      @stream.puts("passed: 0   failed: 0   errors: 0")
+      Assertor::Case.run_all(@cases_list).each do |test_case, results|
+        @passed += add_case_name(test_case, results[:passed])
+        @failed += add_case_name(test_case, results[:failed])
+        results[:exceptions].each do |test, exception|
+          @exceptions["#{test_case}.#{test}"] = exception
+        end
+      end
+
+      @io.puts("passed: #{@passed.size}   failed: #{@failed.size}   errors: #{@errors.size}")
+
+      @failed.each do |t|
+        exception = @exceptions[t]
+        @io.puts
+        @io.puts "Failed: #{t}"
+        @io.puts "   #{exception.inspect}"
+        @io.print "   "
+        @io.puts exception.backtrace.join("\n   ")
+      end
+        
     end
+
+
+  private
+
+    def add_case_name(test_case, tests)
+      tests.collect{ |test| "#{test_case}.#{test}" }
+    end
+
+
   end
 
 end
--- test/reporter_tests/run_test.rb
+++ test/reporter_tests/run_test.rb
@@ -4,14 +4,47 @@ require 'stringio'
 module ReporterTests
 
   class RunTest < Assertor::Case
+
+    def initialize
+      @io = StringIO.new
+    end
   
-    def test_with_empty_list
-      str = StringIO.new
-      reporter = Assertor::Reporter.new(str, [])
+    def should_return_000_with_empty_list
+      reporter = Assertor::Reporter.new(@io, [])
       reporter.run
-      assert(str.string == "passed: 0   failed: 0   errors: 0\n")
+      assert_equals(@io.string, "passed: 0   failed: 0   errors: 0\n")
     end
 
+    def should_return_000_with_empty_case
+      reporter = Assertor::Reporter.new(@io, [make_named_and_ignored_case(self, :Empty)])
+      reporter.run
+      assert_equals(@io.string, "passed: 0   failed: 0   errors: 0\n")
+    end
+
+    def should_return_100_with_passing_case
+      c = make_named_and_ignored_case(self, :Passing)
+      c.send :define_method, :test do end
+      reporter = Assertor::Reporter.new(@io, [c])
+      reporter.run
+      assert_equals(@io.string, "passed: 1   failed: 0   errors: 0\n")
+    end
+
+    def should_return_010_with_failing_case
+      e = Assertor::AssertFailedException.new
+      c = make_named_and_ignored_case(self, :Failing)
+      c.send :define_method, :test do raise e end
+      reporter = Assertor::Reporter.new(@io, [c])
+      reporter.run
+      expected_string = <<-eos
+passed: 0   failed: 1   errors: 0
+
+Failed: #{Failing}.test
+   #{e.inspect}
+   #{e.backtrace.join("\n   ")}
+eos
+      assert_equals(@io.string, expected_string)
+    end
+    
   end
 
 end
